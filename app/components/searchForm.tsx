274c24274a5ff6b66ae929ac1453f4375b5f8fce
stilt0n <mattias.leino@arts.cornish.edu>
2024-01-30 20:10:49 -0500

    refactor: break off SearchForm into its own component

---
+++ app/components/searchForm.tsx
@@ -0,0 +1,32 @@
+import { Form } from "@remix-run/react";
+import { SearchIcon } from "./icons";
+import cn from "classnames";
+
+interface SearchFormProps {
+  defaultValue: string;
+  isSearching?: boolean;
+}
+
+export const SearchForm = ({ isSearching, defaultValue }: SearchFormProps) => {
+  return (
+    <Form
+      className={cn(
+        "flex border-2 border-gray-300 rounded-md",
+        "focus-within:border-primary md:w-80",
+        isSearching ? "animate-pulse" : ""
+      )}
+    >
+      <button className="px-2 mr-1">
+        <SearchIcon />
+      </button>
+      <input
+        defaultValue={defaultValue}
+        type="text"
+        name="query"
+        placeholder="Search shelves..."
+        className="w-full py-3 px-2 outline-none"
+        autoComplete="off"
+      />
+    </Form>
+  );
+};
--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,13 +1,12 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import {
-  Form,
   useLoaderData,
   useNavigation,
   useSearchParams,
 } from "@remix-run/react";
 import { getAllShelves } from "~/models/pantryShelf.sever";
 import { ShelfItems } from "~/components/shelfItems";
-import { SearchIcon } from "~/components/icons";
+import { SearchForm } from "~/components/searchForm";
 import cn from "classnames";
 
 // Remix creates an API layer from the loader and that api layer gets called
@@ -31,25 +30,10 @@ const Pantry = () => {
 
   return (
     <div>
-      <Form
-        className={cn(
-          "flex border-2 border-gray-300 rounded-md",
-          "focus-within:border-primary md:w-80",
-          isSearching ? "animate-pulse" : ""
-        )}
-      >
-        <button className="px-2 mr-1">
-          <SearchIcon />
-        </button>
-        <input
-          defaultValue={searchParams.get("query") ?? ""}
-          type="text"
-          name="query"
-          placeholder="Search shelves..."
-          className="w-full py-3 px-2 outline-none"
-          autoComplete="off"
-        />
-      </Form>
+      <SearchForm
+        defaultValue={searchParams.get("query") ?? ""}
+        isSearching={isSearching}
+      />
       <ul
         className={cn(
           "flex gap-8 overflow-x-auto",
