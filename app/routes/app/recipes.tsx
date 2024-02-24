e4858d4deab0a67844291082abb30d4ef36eb5b3
stilt0n <mattias.leino@arts.cornish.edu>
2024-02-23 23:01:47 -0500

    create recipe page sidebar component

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -1,5 +1,16 @@
-import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import {
+  Form,
+  NavLink,
+  Outlet,
+  useLoaderData,
+  useLocation,
+} from "@remix-run/react";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+  json,
+} from "@remix-run/node";
 import db from "~/db.server";
 import { requireLoggedInUser } from "~/utils/auth.server";
 import {
@@ -9,6 +20,8 @@ import {
 } from "~/components/recipes/wrappers";
 import { RecipeCard } from "~/components/recipes/card";
 import { SearchForm } from "~/components/forms/searchForm";
+import { PlusIcon } from "~/components/icons";
+import { Button } from "~/components/forms/button";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const user = await requireLoggedInUser(request);
@@ -23,21 +36,48 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       },
     },
     select: { name: true, totalTime: true, imageUrl: true, id: true },
+    orderBy: { createdAt: "desc" },
   });
 
   return json({ recipes });
 };
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const user = await requireLoggedInUser(request);
+  const url = new URL(request.url);
+  const recipe = await db.recipe.create({
+    data: {
+      userId: user.id,
+      name: "New Recipe",
+      totalTime: "0 min",
+      imageUrl: "https://via.placeholder.com/150?text=Remix+Recipes",
+      instructions: "",
+    },
+  });
+  url.pathname = `app/recipes/${recipe.id}`;
+  return redirect(url.toString());
+};
+
 const Recipes = () => {
   const data = useLoaderData<typeof loader>();
+  const location = useLocation();
   return (
     <RecipePageWrapper>
       <RecipeListWrapper>
         <SearchForm placeholder="search recipes..." />
+        <Form method="post" className="mt-4">
+          <Button variant="primary" className="flex w-full justify-center">
+            <PlusIcon />
+            <span className="ml-2">Create New Recipe</span>
+          </Button>
+        </Form>
         <ul>
           {data?.recipes.map((recipe) => (
             <li className="my-4" key={recipe.id}>
-              <NavLink reloadDocument to={recipe.id}>
+              <NavLink
+                reloadDocument
+                to={{ pathname: recipe.id, search: location.search }}
+              >
                 {({ isActive }) => (
                   <RecipeCard
                     name={recipe.name}
---
+++ app/routes/app/recipes/$recipeId.tsx
@@ -0,0 +1,26 @@
+// When a route needs to have a dynamic name, you can name the route file starting with a dollar sign
+
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import db from "~/db.server";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const recipe = await db.recipe.findUnique({
+    // The name `recipeId` here comes from the file name
+    where: { id: params.recipeId },
+  });
+
+  return json({ recipe });
+};
+
+const RecipeDetail = () => {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <h1>{data.recipe?.name}</h1>
+    </div>
+  );
+};
+
+export default RecipeDetail;
