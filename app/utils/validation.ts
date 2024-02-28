024579fdabf796801b78fed7f64ef764b78ca84e
stilt0n <mattias.leino@arts.cornish.edu>
2024-02-27 19:02:59 -0500

    support arrays in validate form

--- app/utils/validation.ts
+++ app/utils/validation.ts
@@ -3,13 +3,36 @@ import { z } from "zod";
 
 export type FieldErrors = { [key: string]: string };
 
+type FormFields = {
+  [key: string]: FormDataEntryValue | FormDataEntryValue[];
+};
+
+// Using Object.fromEntries does not work in this case because
+// when the provided array has multiple copies of the same key
+// Object.fromEntries will overwrite the value instead of appending
+// We could determine this from if there are multiple values but we
+// still want single value arrays to be arrays since that's what zod
+// expects. So we will have to mark array fields' names to indicate
+// the are arrays.
+const objectify = (formData: FormData) => {
+  const formFields: FormFields = {};
+  formData.forEach((value, name) => {
+    const isArrayField = name.endsWith("[]");
+    const fieldName = isArrayField ? name.slice(0, -2) : name;
+    if (!(fieldName in formFields)) {
+      formFields[fieldName] = isArrayField ? formData.getAll(name) : value;
+    }
+  });
+  return formFields;
+};
+
 export const validateForm = <T>(
   formData: FormData,
   schema: z.Schema<T>,
   onSuccess: (data: T) => unknown,
   onError: (errors: FieldErrors) => unknown
 ) => {
-  const result = schema.safeParse(Object.fromEntries(formData));
+  const result = schema.safeParse(objectify(formData));
   if (!result.success) {
     const errors: FieldErrors = {};
     for (const issue of result.error.issues) {
